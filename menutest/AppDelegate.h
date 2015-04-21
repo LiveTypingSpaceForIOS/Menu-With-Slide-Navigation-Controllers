1cb33fda960a7c67c39f4c94e9964472053ccb55
Vladimir Vishnyagov <v.vishnyagov@ltst.ru>
2015-04-21 11:39:55 +0600

    Initial Commit

---
+++ menutest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  menutest
+//
+//  Created by Vladimir Vishnyagov on 21.04.15.
+//  Copyright (c) 2015 ltst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ menutest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  menutest
+//
+//  Created by Vladimir Vishnyagov on 21.04.15.
+//  Copyright (c) 2015 ltst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
